7dbeda97bf4d6b3f76a87007bce4587aa6e0c606
Ahmed-Nezar <ahmednezarrr@gmail.com>
2023-12-03 00:26:46 +0200

    add: modifying the html for django environment

--- Django-Environment/Happy_Tails/Happy_Tails/settings.py
+++ Django-Environment/Happy_Tails/Happy_Tails/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Happy_Tails.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Main_Workspace/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "Main_Workspace/static/Main_Workspace",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- Django-Environment/Happy_Tails/Happy_Tails/urls.py
+++ Django-Environment/Happy_Tails/Happy_Tails/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Main_Workspace import views as Main_Workspace_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Main_Workspace_views.home, name='home'),
 ]
